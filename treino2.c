d3b32a67125247483cb42b99fd63a558afb15dba
Gabriel Rosa <gabrielflororosa2007@gmail.com>
2025-09-14 21:53:40 -0300

    Declaracao de variaveis

---
+++ treino2.c
@@ -0,0 +1,18 @@
+//Sintaxe básica de declaração de variáveis em C
+
+#include <stdio.h>
+
+int main(void) {
+    //declaração de variáveis em C
+    int idade;
+    float altura;
+    char letra;
+    double salario;
+
+    //declaracao e inicializacao
+    int dia = 15;
+    float peso = 70.5;
+    char genero = 'M';
+    double distancia = 12345.67;
+    
+}
